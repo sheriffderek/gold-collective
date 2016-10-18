003e0b387d5e978671b4a008e7d78fbb7c63418a
sheriffderek <admin@sheriffderek.consulting>
2016-10-18 12:47:26 -0700

    Formatting

--- app/routes/about.js
+++ app/routes/about.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 	activate() {
-    	this._super();
-    	window.scrollTo(0,0);
-  	},
+  	this._super();
+  	window.scrollTo(0,0);
+  },
 
 	model() {
 		let pageData = this.store.peekRecord('page', 41);
